b8b8210fa343ff8cf0b548257dfbf1c9653a82a8
winderica <winderica@gmail.com>
2017-12-28 17:36:18 +0800

    fixed bugs

--- app.js
+++ app.js
@@ -1,37 +1,38 @@
-var crypto = require('crypto');
-var base64Img = require('base64-img');
-var app = require('express')();
-var expressWs1 = require('express-ws')(app);
-var expressWs2 = require('express-ws')(app);
-var friendWs = expressWs1.app;
-var messageWs = expressWs2.app;
-var webSockets = {};
-var webSocketsMessage = {};
-var bodyParser = require('body-parser');
-var jwt = require('jwt-simple');
-var insert = require('./db/database').insertData;
-var find = require('./db/database').findData;
-var update = require('./db/database').updateData;
+const crypto = require('crypto');
+const base64Img = require('base64-img');
+const app = require('express')();
+const expressWs1 = require('express-ws')(app);
+const expressWs2 = require('express-ws')(app);
+const friendWs = expressWs1.app;
+const messageWs = expressWs2.app;
+const bodyParser = require('body-parser');
+const jwt = require('jwt-simple');
+const insert = require('./db/database').insertData;
+const find = require('./db/database').findData;
+const update = require('./db/database').updateData;
+
+let webSockets = {};
+let webSocketsMessage = {};
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extend: false }));
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
 });
 
-app.post('/user/login', function (req, res) {
+app.post('/user/login', (req, res) => {
     console.log('connected to /user/login');
-    var msg = req.body;
+    const msg = req.body;
     console.log(req.body);
-    var username = msg.username,
+    let username = msg.username,
         password = msg.password;
     password = crypto.createHash('md5').update(password).digest('hex');
-    var obj = {
+    const obj = {
         username: username
     };
-    find('user', obj, function (t) {
+    find('user', obj, t => {
         if (!t[0]) {
             console.log('username is not correct');
             res.status(400).type('application/json').send(JSON.stringify({
@@ -55,16 +56,16 @@ app.post('/user/login', function (req, res) {
         }
     });
 });
-app.post('/user/register', function (req, res) {
+app.post('/user/register', (req, res) => {
     console.log('connected to /user/register');
-    var msg = req.body;
-    var username = msg.username,
+    const msg = req.body;
+    const username = msg.username,
         password = msg.password;
-    var obj = {
+    const obj = {
         username: username,
         password_hash: crypto.createHash('md5').update(password).digest('hex')
     };
-    find('user', {username: username}, function (t) {
+    find('user', {username: username}, t => {
         if (t[0]) {
             console.log('this username has already been registered');
             res.status(400).type('application/json').send(JSON.stringify({
@@ -78,34 +79,41 @@ app.post('/user/register', function (req, res) {
         }
     });
 });
-app.get('/user', function (req, res) {
-    var decoded = jwt.decode(req.headers.authorization, "secret");
-    console.log(decoded);
+app.get('/user', (req, res) => {
+    const decoded = jwt.decode(req.headers.authorization, "secret");
+    console.log('received jwt');
 
     console.log('connected to /user/:username');
-    find('user', {username: decoded.username}, function (s) {
-        if (s.password_hash === decoded.password_hash) {
-            console.log('find user: ', s[0]);
-            res.status(200).type('application/json').send(JSON.stringify(s[0]));
+    find('user', {username: decoded.username}, s => {
+        if (s[0].password_hash === decoded.password_hash) {
+            console.log('find user: ', s[0].username);
+            if (s[0].hasOwnProperty('avatar')) {
+                base64Img.base64(s[0].avatar, (err, data) => {
+                    if (err) throw err;
+                    res.status(200).type('application/json').send(JSON.stringify({username: s[0].username, password_hash: s[0].password_hash, avatar: data}));
+                });
+            } else {
+                res.status(200).type('application/json').send(JSON.stringify({username: s[0].username, password_hash: s[0].password_hash}));
+            }
         } else {
             res.status(400).type('application/json').send(JSON.stringify({name: 'jwt', error: 'jwt isn\'t correct'}))
         }
     });
 });
-app.patch('/user', function (req, res) {
-    var decoded = jwt.decode(req.headers.authorization, "secret");
-    console.log(decoded);
+app.patch('/user', (req, res) => {
+    const decoded = jwt.decode(req.headers.authorization, "secret");
+    console.log('received jwt');
 
     console.log('patch to /user/:username');
-    find('user', {username: decoded.username}, function (s) {
-        if (s.password_hash === decoded.password_hash) {
-            var newData = req.body;
-            var avatar = newData.avatar,
+    find('user', {username: decoded.username}, s => {
+        if (s[0].password_hash === decoded.password_hash) {
+            const newData = req.body;
+            const avatar = newData.avatar,
                 oldPassword = newData.self_password,
                 newPassword = newData.password,
                 username = decoded.username;
             if (oldPassword && newPassword) {
-                var oldHash = crypto.createHash('md5').update(oldPassword).digest('hex'),
+                const oldHash = crypto.createHash('md5').update(oldPassword).digest('hex'),
                     newHash = crypto.createHash('md5').update(newPassword).digest('hex');
                 if (oldHash === newHash) {
                     console.log('same password');
@@ -129,9 +137,10 @@ app.patch('/user', function (req, res) {
                 }
             }
             if (avatar) {
-                base64Img.img(avatar, 'image', Date.now(), function (err, path) {
+                base64Img.img(avatar, 'image', Date.now(), (err, path) => {
                     if (err) throw err;
                     update('user', req.params, {$set: {avatar: path}});
+                    res.status(200).type('application/json').send(JSON.stringify({info: 'avatar updated'}))
                 })
             }
         } else {
@@ -139,101 +148,97 @@ app.patch('/user', function (req, res) {
         }
     });
 });
-app.get('/user/friends', function (req, res) {
-    var decoded = jwt.decode(req.headers.authorization, "secret");
-    console.log(decoded);
-    var username = decoded.username;
-    var friends = [];
-    find('user_relationship', {$or: [{username_a: username}, {username_b: username}]}, function (s) {
-        for (var i = 0; i < s.length; i++) {
+app.get('/user/friends', (req, res) => {
+    const decoded = jwt.decode(req.headers.authorization, "secret");
+    console.log('received jwt');
+    const username = decoded.username;
+    let friends = [];
+    find('user_relationship', {$or: [{username_a: username}, {username_b: username}]}, s => {
+        for (let i = 0; i < s.length; i++) {
             if (s[i].status) {
-                if (s[i].username_a === username) {
-                    friends.push({username: s[i].username_b});
-                } else {
-                    friends.push({username: s[i].username_a});
-                }
+                friends.push({username: s[i].username_a === username? s[i].username_b : s[i].username_b});
                 console.log(friends);
             }
         }
-        function passArray(item) {
-            return function (t) {
+        for (let i = 0; i < friends.length; i++) {
+            find("user", {username: friends[i].username}, t => {
                 if (t.hasOwnProperty('avatar')) {
-                    base64Img.base64(t.avatar, function (err, data) {
+                    base64Img.base64(t.avatar, (err, data) => {
                         if (err) throw err;
-                        item.avatar = data;
+                        friends[i].avatar = data;
+                        res.type('application/json').send(JSON.stringify(friends));
                     });
                 }
-            }
-        }
-        for (i = 0; i < friends.length; i++) {
-            find("user", {username: friends[i].username}, passArray(friends[i]));
+            });
         }
         res.type('application/json').send(JSON.stringify(friends));
     });
 });
 
-friendWs.use('/user/friends/request', function (req, res, next) {
+friendWs.use('/user/friends/request', (req, res, next) => {
     console.log('connected to /user/friends/request');
     next();
 });
-var jwtFriend;
-friendWs.post('/user/friends/request', function (req, res, next) {
+let jwtFriend;
+friendWs.post('/user/friends/request', (req, res, next) => {
     console.log('get post');
-    jwtFriend = req.body.jwt;
-    console.log(jwtFriend);
+    console.log('received jwt');
     res.type('application/json').send(JSON.stringify({info: 'received'}));
     next();
 });
-friendWs.ws('/user/friends/request', function (webSocket) {
-    console.log(jwtFriend);
+friendWs.ws('/user/friends/request', (webSocket) => {
     console.log('websocket connected');
-    var decoded = jwt.decode(jwtFriend, "secret");
+    const decoded = jwt.decode(jwtFriend, "secret");
     console.log(decoded);
-    var uid = decoded.username;
+    const uid = decoded.username;
     webSockets[uid] = webSocket;
     console.log('connected :' + uid + ' in ' + Object.getOwnPropertyNames(webSockets));
-    webSocket.on('message', function (msg) {
+    webSocket.on('message', msg => {
         console.log('received from ' + uid + ': ' + msg);
         msg = JSON.parse(msg);
         if (msg.type === 'history') {
-            find('user_relationship', {username_b: uid}, function (s) {
-                var requests = [];
-                for (var i = 0; i < s.length; i++) {
+            find('user_relationship', {username_b: uid}, s => {
+                let requests = [];
+                for (let i = 0; i < s.length; i++) {
                     if (!s[i].hasOwnProperty('status')) {
                         requests.push({id:s[i].id, username: s[i].username_a});
-                        console.log(requests);
                     }
                 }
-                function passArray(item) {
-                    return function (t) {
-                        if (t.hasOwnProperty('avatar')) {
-                            base64Img.base64(t.avatar, function (err, data) {
-                                if (err) throw err;
-                                item.avatar = data;
-                            });
-                        }
-                    }
-                }
-                for (i = 0; i < requests.length; i++) {
-                    find("user", {username: requests[i].username}, passArray(requests[i]));
-                }
-                webSocket.send(JSON.stringify({type: 'history', requests: requests}));
+                const addImage = (obj) => {
+                    return new Promise(resolve => {
+                        find("user", {username: obj.username}, t => {
+                            if (t.hasOwnProperty('avatar')) {
+                                base64Img.base64(t.avatar, (err, data) => {
+                                    if (err) throw err;
+                                    obj.avatar = data;
+                                    resolve();
+                                });
+                            }
+                        });
+                    })
+                };
+                const promises = requests.map( x => {
+                    return addImage(x);
+                });
+                Promise.all(promises).then(() => {
+                    webSocket.send(JSON.stringify({type: 'history', requests: requests}));
+                })
             });
         } else if (msg.type === 'send_request') {
             console.log(msg);
-            find("user", {username: msg.username}, function (s) {
+            find("user", {username: msg.username}, s => {
                 if (s) {
-                    var to = webSockets[msg.username];
-                    var id = Date.now();
+                    const to = webSockets[msg.username];
+                    const id = Date.now();
                     insert("user_relationship", {id: id, username_a: uid, username_b: msg.username});
                     console.log("inserted " + JSON.stringify({id: id, username_a: uid, username_b: msg.username}));
                     if (to) {
                         console.log('sent to ' + msg.username + ': ' + JSON.stringify(msg));
                         webSocket.send(JSON.stringify({type:'send_request', info: 'received'}));
-                        find("user", {username: uid}, function (t) {
+                        find("user", {username: uid}, t => {
                             t = t[0];
                             if (t.hasOwnProperty('avatar')) {
-                                base64Img.base64(t.avatar, function (err, data) {
+                                base64Img.base64(t.avatar, (err, data) => {
                                     if (err) throw err;
                                     to.send(JSON.stringify({
                                         id: id,
@@ -257,7 +262,7 @@ friendWs.ws('/user/friends/request', function (webSocket) {
                 webSocket.send(JSON.stringify({type:'reject_request', info: 'rejected'}));
             }
             if (msg.username) {
-                find("user_relationship", {username_a: msg.username, username_b: uid}, function (t) {
+                find("user_relationship", {username_a: msg.username, username_b: uid}, t => {
                     if (t) {
                         update("user_relationship", {
                             username_a: msg.username,
@@ -281,63 +286,75 @@ friendWs.ws('/user/friends/request', function (webSocket) {
             webSocket.send(JSON.stringify({info: 'type error'}));
         }
     });
-    webSocket.on('close', function () {
+    webSocket.on('close', () => {
         delete webSockets[uid];
         console.log('deleted: ' + uid);
     })
 });
 
-messageWs.use('/message', function (req, res, next) {
+messageWs.use('/message', (req, res, next) => {
     console.log('connected to /message');
     next();
 });
-var jwtMessage;
-messageWs.post('/message', function (req, res, next) {
+let jwtMessage;
+messageWs.post('/message', (req, res, next) => {
     console.log('get post');
     jwtMessage = req.body.jwt;
-    console.log(jwtMessage);
     res.type('application/json').send(JSON.stringify({info: 'received'}));
     next();
 });
-messageWs.ws('/message', function (webSocket) {
+messageWs.ws('/message', webSocket => {
     console.log('websocket connected');
-    var decoded = jwt.decode(jwtMessage, "secret");
+    const decoded = jwt.decode(jwtMessage, "secret");
     console.log(decoded);
-    var uid = decoded.username;
+    const uid = decoded.username;
     webSocketsMessage[uid] = webSocket;
     console.log('connected :' + uid + ' in ' + Object.getOwnPropertyNames(webSocketsMessage));
-    webSocket.on('message', function (msg) {
+    webSocket.on('message', msg => {
         console.log('received from ' + uid + ': ' + msg);
         msg = JSON.parse(msg);
         if (msg.type === 'history') {
-            var start = msg.start_time;
-            var end = msg.end_time;
-            find('message', {$or: [{from: uid}, {to: uid}]}, function (s) {
-                var messages = [];
-                for (var i = 0; i < s.length; i++) {
-                    console.log('create time', s[i].create_time);
+            const start = msg.start_time;
+            const end = msg.end_time;
+            find('message', {$or: [{from: uid}, {to: uid}]}, s => {
+                let messages = [];
+                for (let i = 0; i < s.length; i++) {
                     if (s[i].create_time > start && s[i].create_time <= end) {
-                        if (s[i].isImage) {
-                            function passArray(item) {
-                                return function (err, data) {
-                                    if (err) throw err;
-                                    messages.push({create_time: item.create_time, from: item.from, to: item.to, image: data});
-                                }
-                            }
-                            base64Img.base64(s[i].content, passArray(s[i]));
-                        } else {
-                            messages.push({create_time: s[i].create_time, from: s[i].from, to: s[i].to, content: s[i].content});
-                        }
+                        messages.push({create_time: s[i].create_time, from: s[i].from, to: s[i].to});
                     }
                 }
-                console.log(messages);
-                webSocket.send(JSON.stringify({type:'history', messages: messages}));
+                const addImage = obj => {
+                    return new Promise(resolve => {
+                        console.log([obj]);
+                        find('message', {create_time: obj.create_time}, t => {
+                            t = t[0];
+                            console.log('find: ', t);
+                            if (t.isImage) {
+                                console.log('path: ', t.content);
+                                obj.image = base64Img.base64Sync(t.content);
+                                resolve();
+                            } else {
+                                console.log('content: ', t.content);
+                                obj.content = t.content;
+                                resolve();
+                            }
+                        });
+                    })
+                };
+                const promises = messages.map( x => {
+                    return addImage(x);
+                });
+                Promise.all(promises).then(() => {
+                    console.log(messages);
+                    console.log('send message');
+                    webSocket.send(JSON.stringify({type: 'history', messages: messages}));
+                });
             });
         } else if (msg.type === 'send_message') {
-            var to = webSocketsMessage[msg.to];
+            const to = webSocketsMessage[msg.to];
             webSocket.send(JSON.stringify({type:'send_message', info: 'received'}));
             if (msg.hasOwnProperty('image')) {
-                base64Img.img(msg.image, 'image', Date.now(), function (err, path) {
+                base64Img.img(msg.image, 'image', Date.now(), (err, path) => {
                     if (err) throw err;
                     insert("message", {create_time: msg.create_time, from: uid, to: msg.to, content: path, isImage: true});
                 });
@@ -368,7 +385,7 @@ messageWs.ws('/message', function (webSocket) {
             webSocket.send(JSON.stringify({type:'send_message', error: 'type error'}));
         }
     });
-    webSocket.on('close', function () {
+    webSocket.on('close', () => {
         delete webSocketsMessage[uid];
         console.log('deleted: ' + uid);
     })
